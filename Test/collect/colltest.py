90db3c29ae31a84304b21389c1057949eb115cf1
Elaina <userelaina@pm.me>
2022-05-30 12:56:08 +0000

    update submodules

--- Test/collect/colltest.py
+++ Test/collect/colltest.py
@@ -11,12 +11,13 @@ command=shlex.split(command)
 s=subprocess.Popen(command,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 
 for _ in range(COUNT):
-    if not _&31:
-        print('colltest',_)
     out=s.stdout.readline().decode('utf8')
     # print(repr(out))
     # time.sleep(1/20)
+    print(out)
     l=[float(i) for i in out.split(',')]
-    assert len(l)==90
+    assert len(l)==45
+    if not _&31:
+        print('SUCC','colltest',_)
 
 print('OK')
