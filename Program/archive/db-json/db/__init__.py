ead9b9e17aaa8f623b16094ca87239b5b1503980
Elaina <userelaina@pm.me>
2022-06-01 06:07:45 +0000

    :truck: Move to GitHub

--- Program/archive/db-json/db/__init__.py
+++
@@ -1,1 +0,0 @@
-from db import admin,device,model
--- Program/archive/db-json/db/model.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from db.__config import BASE
-
-def getBase()->str:
-    return BASE
-
-def setBase(path:str)->None:
-    os.rename(path,BASE)
